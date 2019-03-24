34e6ecb1db15ff640f997ce43a19817c34e688a8
NoodleGG <TGian98@gmail.com>
2019-03-24 17:42:59 +0100

    Section 3 complete
    All instructions from section 3 work correctly.
    
    TBD:
    - Design a network that fits the data, choosing appropriate values for the hyperparameters. Given a fitting network experiment with different combinations of epochs and batchSize

--- DeepLearning/src/main/java/experiments/FunctionExperiment.java
+++ DeepLearning/src/main/java/experiments/FunctionExperiment.java
@@ -1,18 +1,24 @@
 package experiments;
 
-import nl.tue.s2id90.dl.experiment.Experiment;
 import java.io.IOException;
 import nl.tue.s2id90.dl.NN.Model;
+import nl.tue.s2id90.dl.NN.initializer.Gaussian;
 import nl.tue.s2id90.dl.NN.layer.InputLayer;
 import nl.tue.s2id90.dl.NN.layer.SimpleOutput;
 import nl.tue.s2id90.dl.NN.loss.MSE;
+import nl.tue.s2id90.dl.NN.optimizer.Optimizer;
+import nl.tue.s2id90.dl.NN.optimizer.SGD;
 import nl.tue.s2id90.dl.NN.tensor.TensorShape;
+import nl.tue.s2id90.dl.NN.validate.Regression;
+import nl.tue.s2id90.dl.experiment.GUIExperiment;
 import nl.tue.s2id90.dl.input.GenerateFunctionData;
 import nl.tue.s2id90.dl.input.InputReader;
 
-public class FunctionExperiment extends Experiment {
+public class FunctionExperiment extends GUIExperiment {
     // (hyper)parameters
     int batchSize = 32;
+    int epochs = 10;                // number of epochs a training takes
+    double learningRate = 0.01;     // parameter for gradient descent optimization method
 
     public void go() throws IOException {
         // you are going to add code here
@@ -23,13 +29,22 @@ public class FunctionExperiment extends Experiment {
         int inputs = reader.getInputShape().getNeuronCount();
         int outputs = reader.getOutputShape().getNeuronCount();
         Model model = createModel(inputs, outputs);
-        System.out.println(model);
+        System.out.println(model);  // prints summary of the model
+        model.initialize(new Gaussian());   // initializes model weights
 
+        // Training: create and configure SGD && train model
+        Optimizer sgd = SGD.builder()
+                .model(model)
+                .validator(new Regression())
+                .learningRate(learningRate)
+                .build();
+        trainModel(sgd, reader, epochs, 0);
     }
 
     Model createModel(int inputs, int outputs) {
         Model model = new Model(new InputLayer("In", new TensorShape(inputs), true));
         model.addLayer(new SimpleOutput("Out", new TensorShape(inputs), outputs, new MSE(), true));
+        //model.addLayer(new FullyConnected("fc1", new TensorShape(inputs), outputs, new RELU()));
         return model;
     }
 
